c34874755090740774889847616049315b29af34
John Daly <jcdaly97@gmail.com>
2020-05-18 12:40:35 -0700

    created db and completed initial project setup

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const session = require('express-session')
+const userRouter = require('../users/user-router')
+const authRouter = require('../users/auth-router')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/users', userRouter)
+server.use('/api/auth', authRouter)
+
+module.exports = server
